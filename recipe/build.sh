b50633cf228046530cf29236e079f032a9b44cc5
Christian Sachs <c.sachs@fz-juelich.de>
2021-01-11 16:33:06 +0100

    minor fix in build.sh for automated dockerized build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 npm install bower
 cd mycelyso_inspector/static
-../../node_modules/bower/bin/bower install
+../../node_modules/bower/bin/bower --allow-root install
 cd ../..
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
