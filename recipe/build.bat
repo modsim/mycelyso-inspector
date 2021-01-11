40b868ce597199ed3827f93bcae5922505a56a5f
Christian Sachs <c.sachs@fz-juelich.de>
2021-01-11 16:02:27 +0100

    added conda recipe

---
+++ recipe/build.bat
@@ -0,0 +1,5 @@
+npm install bower
+cd mycelyso_inspector/static
+..\..\node_modules\bower\bin\bower install
+cd ..\..
+%PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
---
+++ recipe/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+npm install bower
+cd mycelyso_inspector/static
+../../node_modules/bower/bin/bower install
+cd ../..
+$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
